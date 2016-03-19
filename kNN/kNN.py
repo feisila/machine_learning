c887b952d1faf2009ee865f5142e41d8d6a8455f
feisila <feisila@hotmail.com>
2016-03-19 17:21:24 +0800

    add decision tree and bayes

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -7,8 +7,6 @@ Input:      inX: vector to compare to existing dataset (1xN)
             k: number of neighbors to use for comparison (should be an odd number)
             
 Output:     the most popular class label
-
-@author: feisila
 '''
 from numpy import *
 import operator
